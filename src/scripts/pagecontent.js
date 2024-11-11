b35a33efe0ef886765d6b51023c4aafed8cf1e02
Oliver Tacke <o.tacke@posteo.de>
2024-11-11 13:47:33 +0100

    Use H5P.ColumnXR for pages

--- src/scripts/pagecontent.js
+++ src/scripts/pagecontent.js
@@ -246,7 +246,7 @@ class PageContent extends H5P.EventDispatcher {
 
       // Find sections with tasks and tracks them
       chapter.sections.forEach((section, index) => {
-        if (H5P.Column.isTask(section.instance)) {
+        if (H5P.ColumnXR.isTask(section.instance)) {
           section.isTask = true;
           chapter.maxTasks++;
           chapter.tasksLeft++;
